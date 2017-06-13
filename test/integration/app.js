aa4f9c30997a92fac765cee3c54c995fb6319967
rafael.faria <rafael.faria@infracommerce.com.br>
2017-06-13 17:24:37 -0300

    adicionado parte do teste de 1 livro

--- test/integration/app.js
+++ test/integration/app.js
@@ -1,8 +1,16 @@
 describe('Route Books', ()=>{
-    const defaultBook = {
+    const Books = app.datasource.models.Books,
+    defaultBook = {
         id:1,
         name:"Default Book"
     };
+    beforeEach(done=>{
+        Books.destroy({where:{}})
+        .then(()=> Books.create(defaultBook))
+        .then(()=>{
+            done();
+        })
+    })
 
     describe('Route GET /books',()=>{
         it('Should return book list', done =>{
@@ -11,6 +19,18 @@ describe('Route Books', ()=>{
                 .end((err,res)=>{
                     expect(res.body[0].name).to.be.eql(defaultBook.name);
                     expect(res.body[0].id).to.be.eql(defaultBook.id);
+                    done(err);
+                });
+        });
+    });
+
+    describe('Route GET /books/{id}',()=>{
+        it('Should return a book', done =>{
+                request
+                .get('/books/1')
+                .end((err,res)=>{
+                    expect(res.body.id).to.be.eql(defaultBook.name);
+                    expect(res.body.id).to.be.eql(defaultBook.id);
                     done(err);
                 });
         });
