25a873a4ba63c23d410dd9b810dc29870fc4d7db
Rafael Faria <rafaelr.faria@yahoo.com.br>
2017-07-12 22:08:15 -0300

    fim do teste de unidade do controler com crud

--- controllers/books.js
+++ controllers/books.js
@@ -16,4 +16,31 @@ export default class BooksController {
         .then(result => defaultResponse(result))
         .catch(error => errorResponse(error.message));
     }
+
+    getById(params) {
+        return this.Books.findOne({ where: params })
+        .then(result => defaultResponse(result))
+        .catch(error => errorResponse(error.message));
+    }
+
+    create(data) {
+        return this.Books.create(data)
+        .then(result => defaultResponse(result, 201)) // 201 um recurso foi criado
+        .catch(error => errorResponse(error.message, 422)); // 402 entidade nao pode ser processada
+    }
+
+    update(data, params) {
+        return this.Books.update(data, {
+            where: params,
+        }).then(result => defaultResponse(result))
+        .catch(error => errorResponse(error.message, 422)); // 402 entidade nao pode ser processada
+    }
+
+    delete(params) {
+        return this.Books.destroy({
+            where: params,
+        }).then(result => defaultResponse(result, 204))
+        .catch(error => errorResponse(error.message, 422)); // 402 entidade nao pode ser processada
+    }
+
 }
--- test/unit/controllers/books.js
+++ test/unit/controllers/books.js
@@ -20,4 +20,82 @@ describe('Controllers :Books', () => {
 			.then(response => expect(response.data).to.be.equals(expectedResponse));
         });
     });
+    describe('Get all books: getById()', () => {
+        it('should return a  book', () => {
+            const Books = {
+                findOne: td.function(),
+            };
+
+            const expectedResponse = [{
+                id: 1,
+                name: 'Test Book',
+                created_at: '2017-06-06T23:55:36.6922',
+                updated_at: '2017-06-06T23:55:36.6922',
+            }];
+
+            td.when(Books.findOne({ where: { id: 1 } })).thenResolve(expectedResponse);
+            const booksController = new BooksController(Books);
+            return booksController.getById({ id: 1 })
+			.then(response => expect(response.data).to.be.equals(expectedResponse));
+        });
+    });
+    describe('Create a book: create()', () => {
+        it('should return a  book', () => {
+            const Books = {
+                create: td.function(),
+            };
+            const requestBody = {
+                name: 'Test Book',
+            };
+            const expectedResponse = [{
+                id: 1,
+                name: 'Test Book',
+                created_at: '2017-06-06T23:55:36.6922',
+                updated_at: '2017-06-06T23:55:36.6922',
+            }];
+
+            td.when(Books.create(requestBody)).thenResolve(expectedResponse);
+            const booksController = new BooksController(Books);
+            return booksController.create(requestBody).then((response) => {
+                expect(response.statusCode).to.be.eql(201);
+                expect(response.data).to.be.equals(expectedResponse);
+            });
+        });
+    });
+    describe('Update a book: update()', () => {
+        it('should update a book', () => {
+            const Books = {
+                update: td.function(),
+            };
+            const requestBody = {
+                id: 1,
+                name: 'Test Book Updated',
+            };
+            const expectedResponse = [{
+                id: 1,
+                name: 'Test Book Updated',
+                created_at: '2017-06-06T23:55:36.6922',
+                updated_at: '2017-06-06T23:55:36.6922',
+            }];
+
+            td.when(Books.update(requestBody, { where: { id: 1 } })).thenResolve(expectedResponse);
+            const booksController = new BooksController(Books);
+            return booksController.update(requestBody, { id: 1 }).then((response) => {
+                expect(response.data).to.be.equals(expectedResponse);
+            });
+        });
+	});
+	describe('delete a book: delete()', () => {
+        it('should delete a book', () => {
+            const Books = {
+                destroy: td.function(),
+            };
+
+            td.when(Books.destroy( { where: { id: 1 } })).thenResolve({});
+            const booksController = new BooksController(Books);
+            return booksController.delete({ id: 1 }).then((response) => {
+                expect(response.statusCode).to.be.equals(204);
+            });
+        });
+    });
 });
