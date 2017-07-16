43aae48ff7c9b9ae29055330d9a808caac4a9946
Rafael Faria <rafaelr.faria@yahoo.com.br>
2017-07-16 16:08:25 -0300

    ultimos ajustes de teste de unidade

--- config/config.js
+++ config/config.js
@@ -1,12 +1,12 @@
 export default {
-    database:'books',
-    username:'',
-    password:'',
-    params:{
-        dialect:'sqlite',
-        storage:'books.sqlite',
-        define:{
-            underscored:true
-        }
-    }
-}
+    database: 'books',
+    username: '',
+    password: '',
+    params: {
+        dialect: 'sqlite',
+        storage: `${process.env.NODE_ENV}_books.sqlite`,
+        define: {
+            underscored: true,
+        },
+    },
+};
